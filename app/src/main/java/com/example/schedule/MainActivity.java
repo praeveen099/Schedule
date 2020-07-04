0ee5d6206c37ba35a06e7f4353f46aa07a5f425f
Praeveen Ganesharaja <praeveen099@gmail.com>
2020-07-04 19:13:40 +0800

    Contain a date picker dialog
    Was testing on how to use a date picker dialog
    
    Upon tapping the text area, a dialog will appear
    
    Upon selecting a date, it will put text on dialog

--- app/src/main/java/com/example/schedule/MainActivity.java
+++ app/src/main/java/com/example/schedule/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.schedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    // the text box that will hold the beginning time of the activity
+    TextView beginTimeTxt;
+    private int currentYear, currentMonth, currentDay;
+    Button uploadButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beginTimeTxt= findViewById(R.id.textView);
+        uploadButton = findViewById(R.id.button6);
+
+        uploadButton.setOnClickListener(this);
+
+    }
+
+    // override the current onClick method
+    @Override
+    public void onClick(View v)
+    {
+        if (v == beginTimeTxt)
+        {
+            Calendar currentDateCalendar = Calendar.getInstance();
+            // get the current date from calendar
+            currentYear = currentDateCalendar.get(Calendar.YEAR);
+            currentMonth = currentDateCalendar.get(Calendar.MONTH);
+            currentDay = currentDateCalendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener()
+                    {
+                        // what to do when date is set
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth)
+                        {
+                            beginTimeTxt.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                        }
+                    } , currentYear, currentMonth, currentDay);
+            datePickerDialog.show();
+        }
+
+
     }
 }
