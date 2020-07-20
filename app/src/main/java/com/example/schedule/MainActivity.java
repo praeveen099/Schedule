a8ff06a3b78a6e8e1c89bbdfbf46f1120b2404da
Praeveen Ganesharaja <praeveen099@gmail.com>
2020-07-20 20:13:52 +0800

    Add a divider in the display area
    And also fix the time in the text area of
    
    activity start and end time texts

--- app/src/main/java/com/example/schedule/MainActivity.java
+++ app/src/main/java/com/example/schedule/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 {
     // the text box that will hold the beginning time of the activity
     TextView beginDateTxt, activityStartTimeTxt, activityEndTimeTxt;
+    EditText activityDescriptionTxt;
+    View divider;
     private int currentYear, currentMonth, currentDay, currentHour, currentMinute;
     Button uploadButton;
 
@@ -45,7 +48,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         beginDateTxt= findViewById(R.id.dateToReschedule);
         activityStartTimeTxt = findViewById(R.id.activityStartTime);
         activityEndTimeTxt = findViewById(R.id.activityEndTime);
+        activityDescriptionTxt = findViewById(R.id.activityDescription);
         uploadButton = findViewById(R.id.uploadButton);
+
+        // get the divider
+        divider = findViewById(R.id.divider);
+
 
 
         beginDateTxt.setOnClickListener(this);
@@ -99,7 +107,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         public void onTimeSet(TimePicker view, int hourOfDay,
                                               int minute)
                         {
-                            activityStartTimeTxt.setText(hourOfDay + ":" + minute );
+                            // to ensure that time is displayed properly
+                            if(minute < 10){
+                                String minuteToBeDisplayed = "0" + minute;
+                                activityStartTimeTxt.setText(hourOfDay + ":" + minuteToBeDisplayed );
+                            }
+                            else
+                                activityStartTimeTxt.setText(hourOfDay + ":" + minute );
+
                             // set the text area to have text which is black
                             activityStartTimeTxt.setTextColor(Color.parseColor("#000000"));
 
@@ -125,7 +140,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         public void onTimeSet(TimePicker view, int hourOfDay,
                                               int minute)
                         {
-                            activityEndTimeTxt.setText(hourOfDay + ":" + minute );
+                            // to ensure that time is displayed properly
+                            if(minute < 10){
+                                String minuteToBeDisplayed = "0" + minute;
+                                activityEndTimeTxt.setText(hourOfDay + ":" + minuteToBeDisplayed );
+                            }
+                            else
+                                activityEndTimeTxt.setText(hourOfDay + ":" + minute );
+
+                            // set the text area to have text which is black
                             activityEndTimeTxt.setTextColor(Color.parseColor("#000000"));
 
                         }
@@ -146,7 +169,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             // get the count of children views in the constraint layout
             int count = constraintLayoutToHoldActivities.getChildCount();
-            System.out.println(count);
 
             // if there are child elements
             if (count> 0 )
@@ -154,53 +176,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 // set the previous view before we pressed the upload button
                 View viewToBeRemoved;
 
-                // remove all the views in the layout before we clicked upload
+                // remove all the views in the layout before we clicked upload except divider
                 for (int i = 0; i < count; i++) {
                     viewToBeRemoved = constraintLayoutToHoldActivities.getChildAt(i);
-                    ((ViewGroup) viewToBeRemoved.getParent()).removeView(viewToBeRemoved);
+                    if (!viewToBeRemoved.equals(divider))
+                        ((ViewGroup) viewToBeRemoved.getParent()).removeView(viewToBeRemoved);
                 }
             }
+
 
             // create the set of constraints
             ConstraintSet constraintSet = new ConstraintSet();
 
 
             // create textViews for uploaded date, uploaded start time, end time and description
+            // to be put at the display to hold activities
             TextView uploadedStartTimeTextView =  new TextView(this);
             TextView uploadedEndTimeTextView =  new TextView(this);
-            TextView uploadedDescrptionTextView =  new TextView(this);
+            TextView uploadedDescriptionTextView =  new TextView(this);
             TextView uploadDateTextView = new TextView(this);
+
+
             if((!(beginDateTxt.getText()).equals("Click to set a date")) && (!(activityStartTimeTxt.getText()).equals("Click to set activity start time"))
-            && !(activityEndTimeTxt.getText()).equals("Click to set activity end time"))
+            && !(activityEndTimeTxt.getText()).equals("Click to set activity end time") && !((activityDescriptionTxt.getText()).toString()).matches(""))
             {
+
+                // set divider to be visible
+                divider.setVisibility(View.VISIBLE);
                 uploadDateTextView.setText(beginDateTxt.getText());
+                uploadDateTextView.setTextSize(14);
 
 
-
+                // set the ids for all the text views we are going to create
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
                 {
                     uploadedStartTimeTextView.setId(generateViewId());
                     uploadedEndTimeTextView.setId(generateViewId());
-                    uploadedDescrptionTextView.setId(generateViewId());
+                    uploadedDescriptionTextView.setId(generateViewId());
                     uploadDateTextView.setId(generateViewId());
 
                 }
                 else {
                     uploadedStartTimeTextView.setId(25);
                     uploadedEndTimeTextView.setId(24);
-                    uploadedDescrptionTextView.setId(23);
+                    uploadedDescriptionTextView.setId(23);
                     uploadDateTextView.setId(21);
                 }
+
+                // add the views to the constraint layout
                 constraintLayoutToHoldActivities.addView(uploadDateTextView);
+
 
                 // get the id of the view
                 int idOfTextView = uploadDateTextView.getId();
                 constraintSet.clone(constraintLayoutToHoldActivities);
-                constraintSet.connect(idOfTextView, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0 );
+                constraintSet.connect(idOfTextView, ConstraintSet.TOP, R.id.divider, ConstraintSet.TOP, 0 );
                 constraintSet.connect(idOfTextView, ConstraintSet.START,  ConstraintSet.PARENT_ID, ConstraintSet.START, 0);
                 constraintSet.connect(idOfTextView, ConstraintSet.END,  ConstraintSet.PARENT_ID, ConstraintSet.END, 0);
                 constraintSet.applyTo(constraintLayoutToHoldActivities);
-            } // if the date is  set
+            } // if the entries are all set
 
 
         } // if upload button is pressed
